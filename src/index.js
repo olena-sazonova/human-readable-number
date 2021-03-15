5e97210f0d193473a059657992a3183814b66581
Olena Sazonova <30494770+olena-sazonova@users.noreply.github.com>
2021-03-15 21:21:49 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+    const hundreds = ["", "one hundred ", "two hundred ", "three hundred ", "four hundred ", "five hundred ", "six hundred ", "seven hundred ", "eight hundred ", "nine hundred "];
+    const tens = ["", "not used", "twenty ", "thirty ", "forty ","fifty ", "sixty ", "seventy ", "eighty ", "ninety "];
+    const first_ten = ["ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen", "nineteen "];
+    const numbers = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    if(number == 0) {
+        return "zero";
+    }
+
+    let hundredNumber = Math.floor(number / 100);
+    let result = hundreds[hundredNumber];
+    let tensNumber = Math.floor((number % 100) / 10);
+    let lastDigit = number % 10;
+    if (tensNumber == 1) {
+        result += first_ten[lastDigit];
+    }
+    else {
+        result += tens[tensNumber] + numbers[lastDigit];
+    }
+    return result.trim();
 }
